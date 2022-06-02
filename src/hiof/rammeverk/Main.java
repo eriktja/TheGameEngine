14a8ea92abe2cfc066c119d96e09ce8d45e357a5
eriktja <erik.jarem@gmail.com>
2022-06-02 19:38:29 +0200

    picture in README.md

--- src/hiof/rammeverk/Main.java
+++ src/hiof/rammeverk/Main.java
@@ -1,15 +1,7 @@
 package hiof.rammeverk;
 
-import hiof.rammeverk.TheGameEngine.Window.GameBuilder;
-import hiof.rammeverk.TheGameEngine.Behavior.GameLoopExample;
-import hiof.rammeverk.TheGameEngine.*;
-import hiof.rammeverk.TheGameEngine.GameObjects.*;
-import hiof.rammeverk.TheGameEngine.Behavior.ObjectCreator;
-
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
-
+        // Write your code here
     }
 }
