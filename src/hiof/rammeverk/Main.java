6b2df1dbcf5b8c2b588a1dca81ea00b091e74062
eriktja <erik.jarem@gmail.com>
2022-06-02 20:42:19 +0200

    javadoc

--- src/hiof/rammeverk/Main.java
+++ src/hiof/rammeverk/Main.java
@@ -6,7 +6,6 @@ import hiof.rammeverk.TheGameEngine.*;
 import hiof.rammeverk.TheGameEngine.GameObjects.*;
 import hiof.rammeverk.TheGameEngine.Behavior.ObjectCreator;
 
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
